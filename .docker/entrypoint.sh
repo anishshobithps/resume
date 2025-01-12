61fbd8c47f6865ecbe5f738dcc8ffb18677e47b2
anishshobithps <anish.shobith19@gmail.com>
2025-01-12 05:38:37 +0530

    switch to pdfLatex

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -7,18 +7,18 @@ mkdir -p /latex/output
 # Ensure TeX can find the files
 texhash
 
-# Run XeLaTeX with proper output name
-xelatex -interaction=nonstopmode \
-        -output-directory=/latex/output \
-        -jobname="Anish_Shobith_P_S_Resume" \
-        "main.tex"
+# Run pdfLaTeX with proper output name
+pdflatex -interaction=nonstopmode \
+         -output-directory=/latex/output \
+         -jobname="Anish_Shobith_P_S_Resume" \
+         "main.tex"
 
 # Run again for references if needed
 if grep -q "Rerun to get" "/latex/output/Anish_Shobith_P_S_Resume.log"; then
-    xelatex -interaction=nonstopmode \
-            -output-directory=/latex/output \
-            -jobname="Anish_Shobith_P_S_Resume" \
-            "main.tex"
+    pdflatex -interaction=nonstopmode \
+             -output-directory=/latex/output \
+             -jobname="Anish_Shobith_P_S_Resume" \
+             "main.tex"
 fi
 
 # Copy the final PDF to the mounted volume
